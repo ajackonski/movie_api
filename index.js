fd7dd52a7fadf1f21a45a98301faeca9e3c04076
ajackonski <113149629+ajackonski@users.noreply.github.com>
2024-04-02 18:16:18 -0500

    added some stuff

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+  morgan = require('morgan'),
+  fs = require('fs'),  
+  path = require('path'),
+  bodyParser = require('body-parser')
+const app = express()
+
+// create a write stream (in append mode)
+// a ‘log.txt’ file is created in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
 
 let topMovies = [
     {
@@ -16,21 +27,30 @@ let topMovies = [
     }
   ];
 
-  // GET requests
-app.get('/', (req, res) => {
+  app.get('/', (req, res) => {
     res.send('Welcome to my movie list!');
   });
   
-  app.get('/documentation', (req, res) => {                  
-    res.sendFile('public/documentation.html', { root: __dirname });
-  });
+  app.use('/documentation', express.static('public'));
   
   app.get('/movies', (req, res) => {
     res.json(topMovies);
   });
+
+  app.get('/', (req, res) => {
+    res.send('Welcome to my app!');
+  });
   
-  
-  // listen for requests
+   // listen for requests
   app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
   });
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
