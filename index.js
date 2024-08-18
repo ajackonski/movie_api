dc1f168da87f98f589f4922e2ef02b6d04b83dfc
ajackonski <113149629+ajackonski@users.noreply.github.com>
2024-08-18 10:26:19 -0500

    modified valdiation in /users POST endpoint

--- index.js
+++ index.js
@@ -164,37 +164,46 @@ app.get('/users/:username',passport.authenticate('jwt', { session: false }), asy
 });
 
 //update a user 
-app.put('/users/:username',passport.authenticate('jwt', { session: false }), async (req, res) => {
-  if(req.user.username !== req.params.username){
-    return res.status(400).send('Permission denied');
-}
-  if(req.user.username !== req.params.username){
-    return res.status(400).send('Permission denied');
-}
-  try {
-    const user = await Users.findOne({ username: req.params.username });
-    if (!user) {
-      return res.status(404).send('User not found');
-    }
+app.post('/users', [
+  check('Username', 'Username is required').isLength({ min: 5 }).isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail(),
+  check('Birthday', 'Birthday must be in YYYY-MM-DD format').optional().isISO8601()
+], (req, res) => {
+  
+  // Log the incoming request body
+  console.log('Received request body:', req.body);
 
-    const updatedUser = await Users.findOneAndUpdate(
-      { username: req.params.username },
-      {
-        $set: {
-          username: req.body.username,
-          password: req.body.Password,
-          email: req.body.Email,
-          birthday: req.body.Birthday
-        }
-      },
-      { new: true }
-    );
-
-    res.status(201).json(updatedUser);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error: ' + err);
+  // Check for validation errors
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
   }
+
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
 // Add a new movie to the database
