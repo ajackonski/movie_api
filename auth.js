4985bc4d76e08d4c92c9e0be55c18e9b24e8b951
ajackonski <113149629+ajackonski@users.noreply.github.com>
2024-09-04 17:07:24 -0500

    added log statement for troubleshooting

--- auth.js
+++ auth.js
@@ -21,6 +21,7 @@ let generateJWTToken = (user) => {
 module.exports = (router) => {
     router.post('/login', (req, res) => {
         passport.authenticate('local', { session: false}, (error, user, info) => {
+            console.log(info)
             if (error || !user) {
                 console.log(error)
                 return res.status(400).json({
